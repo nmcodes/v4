b38bfba681c53ac6f54ebb0f459c501f31018d51
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2020-11-09 11:36:11 +0100

    wip

--- src/config.js
+++ src/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'jeanpaul@mulume.me',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/nmcodes',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/jeanpaulngalula',
     },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
+    // {
+    //   name: 'Codepen',
+    //   url: 'https://codepen.io/bchiang7',
+    // },
+    // {
+    //   name: 'Instagram',
+    //   url: 'https://www.instagram.com/bchiang7',
+    // },
     {
       name: 'Twitter',
       url: 'https://twitter.com/bchiang7',
